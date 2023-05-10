60b3e090f019f5aa1827e10482d7b59297f9890c
Peter Bell <peterbell10@live.co.uk>
2023-05-10 18:53:25 +0100

    [inductor] Generate indirect_indexing checks even if optimized out
    Previously `gen_assert_indirect_indexing` was only called on the index
    expressions passed to `ops.load` and `ops.store` which means if the
    variable is optimized out during lowering, we never generate the
    assert. This instead makes `ops.indirect_indexing` eagerly generate
    the assert statement, whether or not it will be used.
    
    ghstack-source-id: b0ae213f3a152775ee7f3d212d97338a765d5513
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100895

--- torch/_inductor/codegen/common.py
+++ torch/_inductor/codegen/common.py
@@ -20,7 +20,6 @@ from ..utils import (
     IndentedBuffer,
     sympy_dot,
     sympy_subs,
-    sympy_symbol,
     unique,
 )
 from ..virtualized import ops, V
@@ -743,7 +742,8 @@ class Kernel(CodeGen):
 
             @staticmethod
             def indirect_indexing(index_var, size):
-                return sympy_symbol(str(index_var))
+                # Skip CSE since this doesn't return an expression
+                return self.indirect_indexing(index_var, size)
 
             @staticmethod
             def load(name: str, index: sympy.Expr):
